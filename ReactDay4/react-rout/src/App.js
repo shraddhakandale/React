13905fd13246f789a15700d8194d0e67ef9ec2f9
Shraddha Kandale <sorkeyy@gmail.com>
2022-02-17 20:31:04 +0530

    react routes

--- ReactDay4/react-rout/src/App.js
+++ ReactDay4/react-rout/src/App.js
@@ -3,16 +3,25 @@ import './App.css';
 import {Link, Route,Routes} from 'react-router-dom';
 import About from './Components/About.js';
 import Contact from './Components/Contact.js';
+import Services from './Components/Services.js';
+;
+
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      {/* <a href="/about">About</a>   here we are not using this to create link because it would reload the page*/}
       <Link to="/about">About</Link><br/>
-      <Link to="/contact">Contact</Link>
+      <Link to="/contact">Contact</Link> <br/>
+      <Link to="/services">Services</Link>
       <Routes>
-        <Route exact path="/about" element={<About/>} />
+        {/* this is how we create the routes and we pass the props in routes */}
+        <Route exact path="/about" element={<About name = "ABOUT"/>} />
+        {/* this is simple route component */}
         <Route exact path="/contact" element={<Contact/>} />
+        {/* this is how we create the routes and we pass the props in routes */}
+        <Route exact path="/services" element={<Services name="SERVICES"/>}/>
       </Routes>
     </div>
   );
--- ReactDay4/react-rout/src/Components/Services.js
+++ ReactDay4/react-rout/src/Components/Services.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-function About(props){
+function Services(props){
     return <h1>This is {props.name} page.</h1>
 }
 
-export default About;
+export default Services;
